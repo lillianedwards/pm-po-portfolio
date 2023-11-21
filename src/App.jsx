a699bd1426ab1768c4e26ebdb5c478501c20fded
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-11-21 09:55:48 -0600

    set up vite shell and file structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./views/Landing";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
